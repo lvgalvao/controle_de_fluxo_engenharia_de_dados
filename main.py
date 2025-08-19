012f36313d3da8d315ab3bfca79888e1b4ccaa7d
Luciano Filho <lvgalvaofilho@gmail.com>
2025-08-18 22:39:28 -0300

    first-commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    
+    if 1 == 1:
+        print("1 é igual a 1")
+
+    elif 1 == 2:
+        print("1 é diferente de 2")
+
+    else:
+        print("Isso é uma LLM no n8n")
+
+
+if __name__ == "__main__":
+    main()
